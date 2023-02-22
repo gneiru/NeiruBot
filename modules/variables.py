41b6a5082fe05cf627db189270e5ab2880b37d6c
gneiru <noelrohi59@gmail.com>
2023-02-22 09:32:21 +0800

    comment: what variables is used for

--- modules/variables.py
+++ modules/variables.py
@@ -1,7 +1,6 @@
-coin = ":coin:" # currency
-desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Accumsan sit amet nulla facilisi. Eu mi bibendum neque egestas congue quisque egestas diam. Euismod quis viverra nibh cras. Vitae nunc sed velit dignissim sodales. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Sed enim ut sem viverra. Vitae tempus quam pellentesque nec nam aliquam. Eget velit aliquet sagittis id. Pretium viverra suspendisse potenti nullam. Sit amet purus gravida quis blandit turpis cursus. Neque convallis a cras semper auctor neque vitae."
-guildName = "Multus"
-Points = {"D": 1, "C": 2, "B": 3, "A": 4, "S": 5, "M": 6}
+coin = ":coin:" # currency emoji
+guildName = "Multus" # guild name
+Points = {"D": 1, "C": 2, "B": 3, "A": 4, "S": 5, "M": 6} # points multiplier for each challenge difficulty
 RankIcons = {
     "D": '<:D:1070133155853516880>',
     "C": '<:C:1070133158974083252>', 
@@ -9,7 +8,7 @@ RankIcons = {
     "A": '<:A:1070133169057189899> ', 
     "S": '<:S:1070133176237817856> ', 
     "M": '<:M:1070133181166129192>'
-}
+} # ranking emojis to view on /leaderboard command
 
 RankImgLink = {
     "D": "https://cdn.discordapp.com/attachments/981476548324495370/1066648150699429928/1.png",
@@ -18,9 +17,9 @@ RankImgLink = {
     "A": "https://cdn.discordapp.com/attachments/981476548324495370/1066648151332769852/4.png",
     "S": "https://cdn.discordapp.com/attachments/981476548324495370/1066648150271602708/5.png",
     "M": "https://cdn.discordapp.com/attachments/981476548324495370/1066648150477119519/6.png"
-}
-AQWUpdatesID = 750741807902892032
-AlinaID = 16480141
+} # ranking image links for embed
+
+# Channels to send on and Roles to ping by the bot //below
 Gifts = {
     "channel" : 1015428489027002448, # daily-gifts-multus
     "role" : 1015422652464242799 #daily-gifts-role
@@ -30,7 +29,8 @@ Gifts1 = {
     "role" : 867911659707047977 #daily-gifts-role-test
 }
 
-NewsChannel1 = 995180817028501524    
-NewsChannel = 904168698766573598 #multus-new-channel
+NewsChannel1 = 995180817028501524    #tests-news-channel
+NewsChannel = 904168698766573598 #multus-news-channel
 
+# Permission for some command for @isNeiruXP decorators
 PermIDS = [666483486735073312,618790940290842625]
